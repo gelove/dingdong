845c85d9262c699af803f18b7b15d895b858ec85
allen <geloves@gmail.com>
2022-04-21 07:45:41 +0800

    fix: 修复并发写入

--- pkg/js/js.go
+++ pkg/js/js.go
@@ -2,22 +2,29 @@ package js
 
 import (
 	"io/ioutil"
+	"sync"
 
 	"github.com/robertkrimen/otto"
 )
 
-var cache = make(map[string][]byte)
+var cache sync.Map
 
 func Call(filePath string, functionName string, args ...interface{}) (result otto.Value, err error) {
-	if _, ok := cache[filePath]; !ok {
-		cache[filePath], err = ioutil.ReadFile(filePath)
-		if err != nil {
-			return
-		}
+	if v, ok := cache.Load(filePath); ok {
+		return call(v.([]byte), functionName, args...)
 	}
 
+	bs, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return
+	}
+	cache.Store(filePath, bs)
+	return call(bs, functionName, args...)
+}
+
+func call(src []byte, functionName string, args ...interface{}) (result otto.Value, err error) {
 	vm := otto.New()
-	_, err = vm.Run(cache[filePath])
+	_, err = vm.Run(src)
 	if err != nil {
 		return
 	}
